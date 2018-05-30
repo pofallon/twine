ec9cc7a7febbf7534e673369c1619f2de095e74c
Paul O'Fallon <paul@ofallonfamily.com>
2018-05-30 14:13:29 -0400

    Refine streams impl

--- commands/lookup.js
+++ commands/lookup.js
@@ -1,11 +1,12 @@
 const through2 = require('through2')
 const ps = require('promise-streams')
-const split2 = require('split2')
-const from2Array = require('from2-array')
+const split = require('split2')
+const parallel = require('parallel-transform')
+const from = require('from2-array')
 const JSONStream = require('JSONStream')
 const CredentialManager = require('../lib/credential-manager')
 const Twitter = require('../lib/twitter')
-const batchStream = require('../lib/batch-stream')
+const batch = require('../lib/batch-stream')
 
 const doLookup = async function (name, api, items, streams = process) {
   let creds = new CredentialManager(name)
@@ -14,9 +15,13 @@ const doLookup = async function (name, api, items, streams = process) {
   let [token, tokenSecret] = await creds.getKeyAndSecret('account')
   twitter.setToken(token, tokenSecret)
   return ps.pipeline(
-    items ? from2Array.obj(items.split(',')) : streams.stdin.pipe(split2()),
-    batchStream(100),
-    ps.map({concurrent: 2}, (data) => twitter.get(`${api}${data.join(',')}`)),
+    items ? from.obj(items.split(',')) : streams.stdin.pipe(split()),
+    batch(100),
+    parallel(2, function (data, next) {
+      twitter.get(`${api}${data.join(',')}`)
+        .then((results) => next(null, results))
+        .catch(next)
+    }),
     through2.obj(function (chunk, enc, next) {
       chunk.forEach((c) => this.push(c))
       next()
