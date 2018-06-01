5d253cb94984e650985a3c3178437b167c948fd3
Paul O'Fallon <paul@ofallonfamily.com>
2018-06-01 15:35:12 -0400

    Refactor parameters

--- commands/lookup.js
+++ commands/lookup.js
@@ -8,14 +8,14 @@ const CredentialManager = require('../lib/credential-manager')
 const Twitter = require('../lib/twitter')
 const batch = require('../lib/batch-stream')
 
-const doLookup = async function (name, api, items, streams = process) {
+const doLookup = async function (api, name, items, inout = process) {
   let creds = new CredentialManager(name)
   let [key, secret] = await creds.getKeyAndSecret('consumer')
   let twitter = new Twitter(key, secret)
   let [token, tokenSecret] = await creds.getKeyAndSecret('account')
   twitter.setToken(token, tokenSecret)
   return ps.pipeline(
-    items ? from.obj(items.split(',')) : streams.stdin.pipe(split()),
+    items ? from.obj(items.split(',')) : inout.stdin.pipe(split()),
     batch(100),
     parallel(2, function (data, next) {
       twitter.get(`${api}${data.join(',')}`)
@@ -27,16 +27,16 @@ const doLookup = async function (name, api, items, streams = process) {
       next()
     }),
     JSONStream.stringify(),
-    streams.stdout
+    inout.stdout
   )
 }
 
 const lookup = {
-  async users (name, users, streams) {
-    await doLookup(name, '1.1/users/lookup.json?screen_name=', users, streams)
+  async users (...args) {
+    await doLookup('1.1/users/lookup.json?screen_name=', ...args)
   },
-  async statuses (name, ids, streams) {
-    await doLookup(name, '1.1/statuses/lookup.json?id=', ids, streams)
+  async statuses (...args) {
+    await doLookup('1.1/statuses/lookup.json?id=', ...args)
   }
 }
 
